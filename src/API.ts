3c25977fae6a986aae2e51834ccb5664b99a8a07
Haven <stackandslash@gmail.com>
2020-08-10 12:29:44 -0700

    stopping off for now

--- src/API.ts
+++ src/API.ts
@@ -4,6 +4,7 @@ export enum Difficulty{
     HARD = "hard"
 }
 
-export const fetchQuizQuestions = async (amount: number, difficulty) =>{
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) =>{
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`
 
 }
